6e5e1b9eae7f6b0814f2824002939cfd875bc770
burse1 <spencerburse@gmail.com>
2024-09-25 14:38:22 -0400

    Yay

--- Lab05.cpp
+++ Lab05.cpp
@@ -1,20 +1,38 @@
-// Lab05.cpp : This file contains the 'main' function. Program execution begins and ends there.
-//
+
 
 #include <iostream>
+#include <vector>
+#include <string>
+
+using namespace std;
+
+
 
 int main()
-{
-    std::cout << "Hello World!\n";
+{   
+    cout << "Input Text to cypher : ";
+    int i;
+    std::string text;
+    getline(cin, text);
+    cout << endl << "Encoded Message: ";
+    vector<char> cypher({ 'V','F','X','B','L','I','T','Z','J','R','P','H','D','K','N','O','W','S','G','U','Y','Q','M','A','C','E' });
+    for (i = 0; i < text.length(); i++) {
+        if (text[i] >= 65 && text[i] <= 90) {
+
+            cout << cypher[text[i] - 65];
+        }
+        else if (text[i] >= 97 && text[i] <= 122) {
+            char character;
+            character = text[i] - 32;
+            character = cypher[character - 65];
+            character = character + 32;
+            cout << character;
+        }
+        else
+            cout << text[i];
+        
+       
+
+    }
 }
 
-// Run program: Ctrl + F5 or Debug > Start Without Debugging menu
-// Debug program: F5 or Debug > Start Debugging menu
-
-// Tips for Getting Started: 
-//   1. Use the Solution Explorer window to add/manage files
-//   2. Use the Team Explorer window to connect to source control
-//   3. Use the Output window to see build output and other messages
-//   4. Use the Error List window to view errors
-//   5. Go to Project > Add New Item to create new code files, or Project > Add Existing Item to add existing code files to the project
-//   6. In the future, to open this project again, go to File > Open > Project and select the .sln file
